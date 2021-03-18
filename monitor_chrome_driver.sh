62e19cea17448d363d0b5637d0c4164d17f2a4d1
Youngil Cho <terzeron@gmail.com>
2021-03-18 17:31:39 +0900

    adjust the threshold from 0 to 5

--- monitor_chrome_driver.sh
+++ monitor_chrome_driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 count=$(ps aux | egrep "(chromedriver|google-chrome)" | grep -v grep | wc -l)
-if [ "$count" -gt 0 ]; then
+if [ "$count" -gt 5 ]; then
     send_msg_to_line.sh "Too many chrome driver processes ($count)"
 fi
 
