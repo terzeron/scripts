9a9b32545d81e91df2ea36bffc6e4c2509aaaec2
Youngil Cho <terzeron@gmail.com>
2021-07-15 14:10:37 +0900

    change diff program from sdiff to icdiff

--- mydiff.sh
+++ mydiff.sh
@@ -1,3 +1,3 @@
 # for git diff
-sdiff --diff-program=colordiff --ignore-tab-expansion --ignore-trailing-space --ignore-all-space --ignore-blank-lines -w$(tput cols) "$2" "$5"
-#colordiff "$2" "$5"
+icdiff --cols=$(tput cols) "$2" "$5"
+exit 0
