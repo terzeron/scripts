c2164cc10d73406dde0c0c79c9065e5db18f8c83
Youngil Cho <terzeron@gmail.com>
2022-02-03 16:25:37 +0900

    add some print-outs modify a stash step

--- merge_to_master.sh
+++ merge_to_master.sh
@@ -2,14 +2,20 @@
 
 echo "git switch develop"
 git switch develop > /dev/null
+echo "git stash"
+git stash > /dev/null
 echo "git push"
 git push > /dev/null
 echo "git switch master"
 git switch master > /dev/null
+echo "git pull"
+git pull > /dev/null
 echo "git merge develop"
 git merge develop > /dev/null
 echo "git push"
 git push > /dev/null
 echo "git switch develop"
 git switch develop > /dev/null
+echo "git stash pop"
+git stash pop > /dev/null
 
