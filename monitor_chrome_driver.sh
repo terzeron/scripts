c520813c6a4f36f2a5d24b006b0b1a76972431f0
Youngil Cho <terzeron@gmail.com>
2024-06-25 11:44:59 +0900

    simplify the pattern of process name

--- monitor_chrome_driver.sh
+++ monitor_chrome_driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-count=$(pgrep -c "(chromedriver|google-chrome)")
+count=$(pgrep -c "chrome")
 if [ "$count" -gt 5 ]; then
-    send_msg_to_gmail.py -s "Monitoring chrome browser and driver" "Too many chrome driver processes ($count)"
+    send_msg_to_gmail.py -s "Monitoring chrome browser and driver" "Too many chrome or chromedriver processes ($count)"
 fi
 
